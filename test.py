8549403b5f835675fe338af106958dd6f056ba64
fzhi <zhi.tianmu@gmail.com>
2018-03-19 14:30:45 -0400

    3/19/18

--- test.py
+++ test.py
@@ -4,10 +4,10 @@ import pandas as pd
 from scipy.stats import kurtosis
 from Signals_Testing import rolling_block_data_fcn,rolling_data_fcn, write
 from ML_functions import getBlendedSignal
-from ML_functions import crossValidate
-from sklearn.ensemble import RandomForestRegressor
+from ML_functions import crossValidate, rollingMultivariateML, featureImportance
+from sklearn.ensemble import RandomForestClassifier
 from sklearn import preprocessing
-from sklearn.model_selection import TimeSeriesSplit
+
 
 '''
 DATA_PATH = "Trading_Input.xlsx"
@@ -55,8 +55,8 @@ pnl = rolling_block_data_fcn(data,sharpe_ratio,gap=30)
 pnl = pd.DataFrame(pnl)
 
 
-'''
 
+'''
 DATA_PATH = "Trading_Input.xlsx"
 TAB_NAME = "Sheet1"
 file  = pd.ExcelFile(DATA_PATH)
@@ -64,12 +64,33 @@ data = file.parse(TAB_NAME)
 
 data = data.dropna()
 
-ml_out = getBlendedSignal(data, RandomForestRegressor, gap=500)
 
-ml_out = pd.DataFrame(ml_out)
+#ml_out = getBlendedSignal(data, RandomForestRegressor, gap=500)
+
+#ml_out = pd.DataFrame(ml_out)
 
 #write(ml_out,'ml_output.xlsx','rf')
 
-X = data.drop(['tenyear','change'])
-Y = data['change']
-score = crossValidate(X,Y,RandomForestRegressor)
+
+
+
+X = data.drop(['tenyear','upordown','change'],axis=1)
+Y = data['upordown']
+
+X = X.values
+Y = Y.values
+
+#X = np.array([[1,2,3],[4,5,6],[7,8,9],[10,11,12]])
+#Y = np.array([100,101,102,103])
+
+dataML = data.drop(['tenyear','change'],axis=1)
+
+model_kwargs = {'n_estimators':5}
+#score = crossValidate(X,Y,0.7,RandomForestClassifier,**model_kwargs)
+
+kwargs = {'trainSplit':0.7, 'model_fcn': RandomForestClassifier, 'model_kwargs': model_kwargs}
+scores = rollingMultivariateML(dataML,100,crossValidate, **kwargs)
+print(scores)
+
+FIs = rollingMultivariateML(dataML,100,featureImportance, **kwargs)
+print(FIs)
