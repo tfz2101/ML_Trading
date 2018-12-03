facb494243b9c6bd1e4e7315f1cfd727a4b97308
fzhi <zhi.tianmu@gmail.com>
2018-12-03 17:08:19 -0500

    3/19/18

--- CryptoPanic_API.py
+++ CryptoPanic_API.py
@@ -17,35 +17,49 @@ FILTER = '&currencies=BTC&filter=important'
 
 db = TinyDB('CryptoPanic_DB.json')
 
+#RECORD SENTIMENT/POSITIVES/NEGATIVES OF LAST 20 IMPORTANT ARTICLES IN 5 MIN INTERNVALS
+INTERVAL = 300
 
-for i in range(0, 2):
+for i in range(0, 5000):
 
-    now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S%z')
     #Important Cateory of news sentiment
     POLARITY = []
     POLARITY_ADJ = []
+    PRINT_TIMES  = []
+    POSITIVES = []
+    NEGATIVES = []
     url = "https://cryptopanic.com/api/posts/?auth_token=" + KEY + FILTER
     response = requests.get(url).json()
     results = response['results']
     print(results)
-    print('number of items', len(results))
     for item in results:
         title = item['title']
         sentiment = TextBlob(title).sentiment
         POLARITY.append(sentiment.polarity)
         POLARITY_ADJ.append(float(sentiment.polarity) * sentiment.subjectivity)
 
+        positive = item['votes']['positive']
+        POSITIVES.append(positive)
+        negative = item['votes']['negative']
+        NEGATIVES.append(negative)
+        print_time = str(item['created_at'])
+        PRINT_TIMES.append(print_time)
+
     pol_mean = np.array(POLARITY).mean()
     pol_adj_mean  = np.array(POLARITY_ADJ).mean()
+    latest_created_time =  PRINT_TIMES[0]
+    avg_positives = np.array(POSITIVES).mean()
+    avg_negatives =  np.array(NEGATIVES).mean()
 
-
+    now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S%z')
     print(now)
-    db.insert({'designation': 0, 'time': now, 'avg_polarity': pol_mean, 'avg_polarity_adj': pol_adj_mean})
 
-    time.sleep(300)
+    db.insert({'designation': 0, 'time': now, 'latest_created_time': latest_created_time, 'avg_positives': avg_positives, 'avg_negatives': avg_negatives, 'avg_polarity': pol_mean, 'avg_polarity_adj': pol_adj_mean})
+
+    time.sleep(INTERVAL)
 
 
-
+#RETRIEVE ALL RECORDS
 '''
 User = Query()
 search_res = db.search(User.designation == 0)
